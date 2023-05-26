004c171b42aa7332e3822217d492d630df03f6ae
Marcin Adamiak <marcin_89@outlook.com>
2023-05-26 22:10:01 +0200

    Try to add function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Timer from './components/Timer/Timer';
 import Button from './components/Button/Button';
 import Container from './components/Container/Container';
+import { useState } from 'react';
 
 const App = () => {
+  const [time, setTime] = useState(0);
+
+  const [interval, setInterval] = useState(null);
+
   return (
     <Container>
       <Timer />
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,7 +1,25 @@
 import styles from './Timer.module.scss';
 
-const Timer = () => {
-  return;
+const Timer = (props) => {
+  const msToHMS = (ms) => {
+    // 1- Convert to seconds:
+    let seconds = ms / 1000;
+    // 2- Extract hours:
+    const hours = parseInt(seconds / 3600);
+    // 3,600 seconds in 1 hour
+    seconds = seconds % 3600; // seconds remaining after extracting hours
+    // 3- Extract minutes:
+    const minutes = parseInt(seconds / 60);
+    // 60 seconds in 1 minute
+    // 4- Keep only seconds not extracted to minutes:
+    const sec = Math.floor(seconds % 60);
+
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${sec
+      .toString()
+      .padStart(2, '0')}:${ms.toString().slice(-3).padStart(3, '0')}`;
+  };
+
+  return <div>HH:mm:ms</div>;
 };
 
 export default Timer;
